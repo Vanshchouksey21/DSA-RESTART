c4617108836711620b6e40b3a59d4e386d33151f
Vanshchouksey21 <vanshchouksey2175@gmail.com>
2025-12-14 23:47:53 +0530

    first largest and the smallest in an array with climits file

--- Array/largestAndSmall.cpp
+++ Array/largestAndSmall.cpp
@@ -1,4 +1,39 @@
 // Find largest & smallest element in array
 
 
-#include<iostream>
+    #include<iostream>
+    #include<climits>
+    using namespace std ;
+    void smallLar(int* arr , int n ){
+        int lar = INT_MIN ;
+        int min = INT_MAX;
+        for(int i = 0 ; i < n  ;++i){
+            if(arr[i] > lar){
+                lar = arr[i];
+            }
+             if (arr[i]<min){
+                min = arr[i];
+            }
+
+        }
+        cout<<endl;
+        cout<<lar<<endl<<min;
+    }
+    int main(){
+        int n ;
+        cin>>n;
+        int *arr = new int[n];
+
+        for(int i = 0 ; i < n ; ++i){
+            cout<<"enter the values in an array :";
+            cin>>arr[i];
+        }
+         cout<<" values of an array :"<<endl;
+            for(int i = 0 ; i < n ; ++i){
+           
+            cout<<arr[i];
+        }
+
+        smallLar(arr , n);
+
+    }
